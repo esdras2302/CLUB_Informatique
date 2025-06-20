0383edf1d1951e9a940e99305800a982e4d626df
esdras2302 <esdrassomitondji@gmail.com>
2025-06-20 17:51:30 +0100

    messagesw

---
+++ App.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Titre de l'application
+st.title("Table de Multiplication")
+
+# Description
+st.write("Choisissez un nombre pour afficher sa table de multiplication de 1 à 10.")
+
+# Entrée utilisateur
+number = st.number_input("Entrez un nombre", min_value=1, max_value=-1, value=1, step=1)
+
+# Afficher la table de multiplication
+st.header(f"Table de multiplication de {number}")
+for i in range(1, 11):
+    result = number * i
+    st.write(f"{number} × {i} = {result}")
